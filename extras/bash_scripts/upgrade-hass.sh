e90d671873ff6d851d2b60f7b29c8416b5db4a12
mf_social <marcforth.social@gmail.com>
2017-04-14 23:48:38 +0100

    added script to upgrade HA - with comments

--- extras/bash_scripts/upgrade-hass.sh
+++ extras/bash_scripts/upgrade-hass.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -x
+
+##########################################
+## This script stops HA, upgrades the OS##
+## upgrades HA then reboots the device  ##
+##########################################
+
 sudo systemctl stop home-assistant.service
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove
 source /srv/hass/hass_venv/bin/activate
