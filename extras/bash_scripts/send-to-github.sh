078040863c118d985cda207fcbf2727d6bb8664e
mf_social <marcforth.social@gmail.com>
2017-04-14 23:59:15 +0100

    Removed prompt from comment box

--- extras/bash_scripts/send-to-github.sh
+++ extras/bash_scripts/send-to-github.sh
@@ -8,7 +8,7 @@
 cd /home/hass/.homeassistant
 git add .
 git status
-echo -n "Enter the Description for the Change: " [Minor Update]
+echo -n "Enter the Description for the Change: "
 read CHANGE_MSG
 git commit -m "${CHANGE_MSG}"
 git push origin master
