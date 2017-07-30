38b02e35c8025f57b2119aafe58b3e8cba61f133
mf_social <marcforth.social@gmail.com>
2017-07-30 14:57:07 +0100

    minor changes for theming tests, other breaking changes in updates and update script itself.

--- extras/bash_scripts/upgrade-hass.sh
+++ extras/bash_scripts/upgrade-hass.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -x
 
 ##########################################
-## This script stops HA, upgrades the OS##
+## This script upgrades the OS on my pi,##
 ## upgrades HA then reboots the device  ##
 ##########################################
 
-sudo systemctl stop home-assistant.service
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove
 source /srv/hass/hass_venv/bin/activate
 pip3 install --upgrade homeassistant
