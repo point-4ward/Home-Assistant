38abf56a1e151df2c2bc9e49a40699fb356a5040
mf_social <marcforth.social@gmail.com>
2017-11-20 18:50:32 +0000

    updates from todo list

--- extras/bash_scripts/upgrade-hass.sh
+++ extras/bash_scripts/upgrade-hass.sh
@@ -6,6 +6,6 @@
 ##########################################
 
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove
-source /srv/hass/hass_venv/bin/activate
+source ~/bin/activate
 pip3 install --upgrade homeassistant
 sudo reboot
