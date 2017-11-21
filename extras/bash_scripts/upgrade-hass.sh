57f319e7e32730e68456e5e3220118812e6e3387
mf_social <marcforth.social@gmail.com>
2017-11-21 13:50:07 +0000

    upgrade to 58.1, new cca group for downstairs, fixed some issues

--- extras/bash_scripts/upgrade-hass.sh
+++ extras/bash_scripts/upgrade-hass.sh
@@ -6,6 +6,6 @@
 ##########################################
 
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove
-source ~/bin/activate
+source ~/srv/bin/activate
 pip3 install --upgrade homeassistant
 sudo reboot
