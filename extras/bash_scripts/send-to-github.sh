c7834682702c6975e2030103707e0f131c25efbd
Marc Forth <marcforth.social@gmail.com>
2017-12-10 20:39:52 +0000

    Upload (#108)
    * Push from local.
    
    * Push from local.

--- extras/bash_scripts/send-to-github.sh
+++ extras/bash_scripts/send-to-github.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-###################################
+####################################
 ## This script pushes my selected ##
-## files to my github repo        ##
+## files to my github repo on a   ##
+## new branch called 'upload'     ##
 ####################################
 
 cd /home/hass/.homeassistant
-git checkout -b upload
-git checkout upload
 git add .
 git status
-echo -n "Enter the Description for the Change: "
-read CHANGE_MSG
-git commit -m "${CHANGE_MSG}"
-git push - u origin upload
+git checkout -b upload
+git checkout upload
+git commit -m "Push from local."
+git push origin upload
 exit
