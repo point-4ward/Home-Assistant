769979d77a4259d014daf54c61d381707330cf31
Marc Forth <marcforth.social@gmail.com>
2017-12-18 05:02:41 +0000

    Update upgrade-hass.sh

--- extras/bash_scripts/upgrade-hass.sh
+++ extras/bash_scripts/upgrade-hass.sh
@@ -5,6 +5,9 @@
 ## upgrades HA then reboots the device  ##
 ##########################################
 
+cd /home/hass/.homeassistant/
+git checkout master
+git pull
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove
 source ~/srv/bin/activate
 pip3 install --upgrade homeassistant
