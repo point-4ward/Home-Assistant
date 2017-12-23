f4ea5893de4085b2c65255c24c28db29d913631f
mf_social <marcforth.social@gmail.com>
2017-12-23 11:15:39 +0000

    Push from local.

---
+++ extras/bash_scripts/sync-dropbox.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+#################################################
+## This script runs the dropbox_sync.py script ##
+## as I could not get it to run directly!      ##
+#################################################
+
+cd /home/hass/.homeassistant/
+python dropbox_sync.py
+exit
