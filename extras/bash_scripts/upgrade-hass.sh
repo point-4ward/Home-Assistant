db4de3163819fbeb973b5a09cb9957bafed96a82
Marc Forth <marcforth.social@gmail.com>
2018-01-28 13:34:23 +0000

    minor tweaks after 0.62 update

--- extras/bash_scripts/upgrade-hass.sh
+++ extras/bash_scripts/upgrade-hass.sh
@@ -14,6 +14,6 @@ git branch -D upload
 git fetch origin master
 git reset --hard origin/master
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove
-source ~/srv/bin/activate
+source /srv/homeassistant/bin/activate
 pip3 install --upgrade homeassistant
 sudo reboot
