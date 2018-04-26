fc402493d6fd0fe339b06495e1cec13330ef084c
Marc Forth <marcforth.social@gmail.com>
2018-04-26 01:56:57 +0100

    Improve backup to dropbox (#484)

--- extras/bash_scripts/sync-dropbox.sh
+++ extras/bash_scripts/sync-dropbox.sh
@@ -5,6 +5,8 @@
 ## as I could not get it to run directly!      ##
 #################################################
 
-cd /home/homeassistant/.homeassistant/private/
+cd /home/homeassistant/.homeassistant/
+rsync -avz --exclude home-assistant.log --exclude home-assistant_v2.db  . ../homeassistant_backup/
+cd private/
 python dropbox_sync.py
 exit
