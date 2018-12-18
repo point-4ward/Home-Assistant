2beeb4b1692b91db05044c39a32c32209766d14f
Marc Forth <marcforth.social@gmail.com>
2018-12-18 11:47:59 +0000

    minor changes (#618)

--- .bin/bash_scripts/zip-dropbox.sh
+++ .bin/bash_scripts/zip-dropbox.sh
@@ -14,7 +14,7 @@ _now=$(date +"%d_%m_%Y")
 _file="backup_$_now.zip"
 
 cd /opt
-rsync -avz --delete --exclude=docker/mosquitto/mosquitto.db --exclude=docker/mariadb/* . backup_tmp/
+rsync -avz --delete --exclude=docker/mosquitto/mosquitto.db --exclude=docker/mariadb/* --exclude=backup_zip/* . backup_tmp/
 zip -r backup_zip/$_file backup_tmp/
 cd dropbox/
 python dropbox_sync.py
